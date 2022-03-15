e4f1eeb47c9ef076fc90bd1da388791aa0663cc7
bekhanhihi <92852289+bekhanhihi@users.noreply.github.com>
2022-03-16 00:24:58 +0700

    Delete quanlybanhang directory

--- quanlybanhang/application/views/common/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="center">   
-		<span class="white">PHẦN MỀM QUẢN LÝ BÁN HÀNG v1</span>
-
-		<a target="_blank" data-event-category="Manage" data-event-action="support-page"><span class="white"> Hotline: 0169.6052574</span></a>
-		<span class="white"> COPYRIGHT © BANHMJ VLOG</span>
-</div>
